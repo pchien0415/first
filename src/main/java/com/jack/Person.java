18a11bf4a4ffd113bffc78c715380ba709e53cac
pchien0415 <jackdd0415@yahoo.com.tw>
2020-08-19 19:46:46 +0800

    Initial commit

---
+++ src/main/java/com/jack/Person.java
@@ -0,0 +1,7 @@
+package com.jack;
+
+public class Person {
+    public void hello(){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/kotlin/com/jack/Hello.kt
@@ -0,0 +1,13 @@
+package com.jack
+
+fun main() {
+    //println("Hello World")
+    val h = Human()
+    h.hello()
+}
+
+class Human(){
+    fun hello(){
+        println("Hello Kotlin")
+    }
+}
